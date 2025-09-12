e4d6d904738e6399d7502cdb8e591bac0d14cbb3
Chen Jiale <123545356+Salinoia@users.noreply.github.com>
2025-09-12 17:08:24 +0800

    Add yaml-cpp include dirs to tests

--- modules/http/HttpServer.h
+++ modules/http/HttpServer.h
@@ -11,12 +11,19 @@
 
 class HttpServer {
 public:
+    using HttpCallback = std::function<void(HttpRequest&, HttpResponse&)>;
+
     HttpServer(EventLoop* loop, const InetAddress& listenAddr, const std::string& name, TcpServer::Option option = TcpServer::kNoReusePort);
 
     Router& router() { return router_; }
 
     void setThreadNum(int numThreads) { server_.setThreadNum(numThreads); }
     void start();
+
+    void setHttpCallback(const HttpCallback& cb) { httpCallback_ = cb; }
+
+    // Expose request handling for tests
+    void handleRequestForTest(const TcpConnectionPtr& conn, HttpRequest& req) { onRequest(conn, req); }
 
 private:
     void onConnection(const TcpConnectionPtr& conn);
@@ -26,4 +33,5 @@ private:
     TcpServer server_;
     Router router_;
     std::unordered_map<TcpConnection*, HttpContext> contexts_;
+    HttpCallback httpCallback_;
 };
